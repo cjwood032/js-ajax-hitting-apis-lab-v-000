fe7ab4f048ac22312b5f0ee174d99436338516e7
Christopher Wood <cjwood032@gmail.com>
2018-09-22 14:29:12 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -41,5 +41,18 @@ function getCommits(cm) {
 }
 
 function displayCommits(){
-
+  const commits = JSON.parse(this.responseText);
+const commitsList = `<ul>${commits
+  .map(
+    commit =>
+      '<li><h3>' +
+      commit.commit.author.name +
+      ' (' +
+      commit.author.login +
+      ')</h3>' +
+      commit.commit.message +
+      '</li>'
+  )
+  .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
